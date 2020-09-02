41d13d5b5df25379d8622ff70c4c6b5ea437c119
karleighponnock <karleighponnock@gmail.com>
2020-09-02 16:39:30 -0400

    if statement with elif

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 # conditional statements in python
 
-check = True
+check = "Hamburger"
 
 if check == False:
     print("It is false")
+elif check == "Hamburger":
+    print("Yummmm, hamburgers")
+elif check == "Yo":
+    print("Hello")
 else:
     print("It is actually equal to True")
