4a4eaed25eba3bb17cc1c91a658af1f4a9a3a638
karleighponnock <karleighponnock@gmail.com>
2020-09-02 16:37:29 -0400

    simple if else statement

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
+# conditional statements in python
 
-# define function
-def do_math(num1, num2):
-    return num1 + num2
+check = True
 
-math1 = do_math(5, 7)
-math2 = do_math(11,34)
-
-print("first sum is", math1, "secoond sum is", math2)
+if check == False:
+    print("It is false")
+else:
+    print("It is actually equal to True")
