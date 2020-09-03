64315ab4dc14d1cfdf6109c44bf23f6b37b949d0
karleighponnock <karleighponnock@gmail.com>
2020-09-03 13:57:24 -0400

    calculator continues on last operation until quit

--- main.py
+++ main.py
@@ -10,15 +10,30 @@ previous = 0
 run = True
 
 def performMath():
+    #access variables globally
     global run
     global previous
-    equation = input("Enter equation:")
+    #if just starting ask for equation
+    equation = ""
+    if previous == 0:
+        equation = input("Enter equation:")
+    #if an operation has been done print the previous answer
+    else:
+        equation = input(str(previous))
+    #if user tyoes exit end app
     if equation == 'quit':
         run = False
+    #remove everything other than numbers or operators from user input
+    #evaluate it and print new answer
     else:
         equation = re.sub('[a-zA-Z,.:()" "]','',equation)
-        previous = eval(equation)
-        print("You typed", previous)
+
+        if previous == 0:
+            previous = eval(equation)
+        else:
+            previous = eval(str(previous) + equation)
+        
+     
 
 while run:
     performMath()
