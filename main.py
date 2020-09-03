60a33a04aa011579d526008707085eaedf762a4b
karleighponnock <karleighponnock@gmail.com>
2020-09-03 13:28:52 -0400

    make run global. if user types quit the program will stop running

--- main.py
+++ main.py
@@ -4,13 +4,18 @@
 import re
 
 print("Python Calculator")
+print("Type `quit` to exit\n")
 
 previous = 0
 run = True
 
 def performMath():
+    global run
     equation = input("Enter equation:")
-    print("You typed", equation)
-    
+    if equation == 'quit':
+        run = False
+    else:
+         print("You typed", equation)
+
 while run:
     performMath()
